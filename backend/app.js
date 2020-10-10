81ded85ede2c2fdbeb155a171c7955013e7f40bf
Anton Imanov <antony.imanov@gmail.com>
2020-10-10 09:54:40 +0000

    Added main api file and few folders

---
+++ backend/app.js
@@ -0,0 +1,27 @@
+const 	express     = require("express"),
+	    app         = express(),
+	    mongoose    = require("mongoose");
+    
+//requiring routes
+//const	 indexRoutes    = require("./routes/index");
+
+const dbUrl = process.env.DATABASEURL || "mongodb://localhost:27017/chall_db";
+
+mongoose.connect(dbUrl, { 
+	useUnifiedTopology: true,                         
+	useNewUrlParser: true,                            
+})
+.then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log("DB Connection Error: " + err.message);
+});
+
+// app.use("/", indexRoutes);
+
+app.get("/",function(req,res){
+	res.send("Main page here");
+})
+
+app.listen(3000, function(){
+	console.log('App successfully has started');
+});
