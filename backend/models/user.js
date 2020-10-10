af46b3e972dc3c84b2b197b1ecb023aa59cc95f9
Mishiko <webshifty@gmail.com>
2020-10-10 17:38:38 +0300

    v0.0.1 Add task model Add user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -2,10 +2,14 @@ var mongoose = require("mongoose");
 var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
-    username: String,
-    password: String
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String,
+    vipStatus: Boolean,
+    value: Int32Array
 });
 
 UserSchema.plugin(passportLocalMongoose)
 
-module.exports = mongoose.model("User", UserSchema);
+module.exports = mongoose.model("Users", UserSchema);
