ae1a90efab48330076ca5dbabb0ba27978d44219
Mishiko <webshifty@gmail.com>
2020-10-11 03:40:55 +0300

    v0.0.7 Fix routes

--- server/app.js
+++ server/app.js
@@ -49,7 +49,7 @@ app.get('/api/getList', (req,res) => {
 app.use("/", indexRoutes);
 
 app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../public/index.html')));
 
 app.listen(PORT, function(){
 	console.log(`server started on port ${PORT}`);
