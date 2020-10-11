6e38615f9e174af12eeded01cdb722c00bb6ea03
Mishiko <webshifty@gmail.com>
2020-10-11 03:30:05 +0300

    v0.0.7 Fix routes

--- server/app.js
+++ server/app.js
@@ -15,7 +15,7 @@ const	indexRoutes    	= require("../routes");
 
 const dbUrl = "mongodb+srv://dbuser1:topcodeR37@Cluster0.vvlrr.mongodb.net/cluster0?retryWrites=true&w=majority"
 
-const buildPath = path.join(__dirname, '..', 'build');
+const buildPath = path.join(__dirname, '..', 'src');
 app.use(express.static(buildPath));
 
 const PORT = process.env.PORT || 5000;
@@ -43,10 +43,16 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
-app.use(express.static(path.join(__dirname, 'public')));
-app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/api/getList', (req,res) => {
+	var list = ["item1", "item2", "item3"];
+	res.json(list);
+	console.log('Sent list of items');
+});
 
 app.use("/", indexRoutes);
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 
 app.listen(PORT, function(){
 	console.log(`server started on port ${PORT}`);
